31032cf0c9ae21731bd6d239e7df2906c60ef31a
GlazovDaniil <d.glazow2016@yandex.ru>
2024-03-14 22:52:42 +0300

    0.2.5.1

--- events/events/asgi.py
+++ events/events/asgi.py
@@ -8,9 +8,22 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from django.urls import path
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'events.settings')
 
-application = get_asgi_application()
+# application = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    'websocket': AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter([
+                # [...]
+            ])
+        )
+    )
+})
--- events/events/settings.py
+++ events/events/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'channels',
     'corsheaders',
 
     'allauth',
@@ -86,6 +87,17 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'events.wsgi.application'
+
+ASGI_APPLICATION = 'events.asgi.application'
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            'hosts': ['127.0.0.1', '6379'],
+        },
+    },
+}
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
