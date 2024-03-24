96e27f6cb98de5532091f588846e1373745295b5
GlazovDaniil <d.glazow2016@yandex.ru>
2024-03-24 17:15:44 +0300

    0.2.5.16

--- events/meetings/views.py
+++ events/meetings/views.py
@@ -180,39 +180,43 @@ class TimetableCreate(generics.CreateAPIView):
     serializer_class = TimetableSerializer
 
     def post(self, request, *args, **kwargs):
-        place = int(request.POST.get("place"))
-        event_date = request.POST.get("event_date")
-        start_time = request.POST.get("start_time")
-        end_time = request.POST.get("end_time")
+        try:
+            place = request.POST.get("place")
+            event_date = request.POST.get("event_date")
+            start_time = request.POST.get("start_time")
+            end_time = request.POST.get("end_time")
 
-        timetables = Timetable.objects.filter(place=place, event_date=event_date)
+            timetables = Timetable.objects.filter(place=place, event_date=event_date)
 
-        s_t = start_time.split(':')
-        e_t = end_time.split(':')
+            s_t = start_time.split(':')
+            e_t = end_time.split(':')
 
-        start = datetime.time(int(s_t[0]), int(s_t[1]))
-        end = datetime.time(int(e_t[0]), int(e_t[1]))
-        if start < end:
-            marker = False
-            counter = 0
-            for timetable in timetables:
-                # print(timetable)
-                counter += 1
-                if not ((timetable.start_time <= start <= timetable.end_time)
-                        or (timetable.start_time <= end <= timetable.end_time)
-                        or (start <= timetable.start_time and end >= timetable.end_time)):
-                    marker = True
-                    break
-            if marker or counter == 0:
-                request.data._mutable = True
-                request.data['author'] = request.user.id
-                request.data._mutable = False
-                return self.create(request, *args, **kwargs)
+            start = datetime.time(int(s_t[0]), int(s_t[1]))
+            end = datetime.time(int(e_t[0]), int(e_t[1]))
+            if start < end:
+                marker = False
+                counter = 0
+                for timetable in timetables:
+                    # print(timetable)
+                    counter += 1
+                    if not ((timetable.start_time <= start <= timetable.end_time)
+                            or (timetable.start_time <= end <= timetable.end_time)
+                            or (start <= timetable.start_time and end >= timetable.end_time)):
+                        marker = True
+                        break
+                if marker or counter == 0:
+                    request.data._mutable = True
+                    request.data['author'] = request.user.id
+                    request.data._mutable = False
+                    return self.create(request, *args, **kwargs)
+                else:
+                    raise MyCustomException(detail={"error": "Невозможно записать на эту дату и время, так как они заняты"},
+                                            status_code=status.HTTP_400_BAD_REQUEST)
             else:
-                raise MyCustomException(detail={"error": "Невозможно записать на эту дату и время, так как они заняты"},
+                raise MyCustomException(detail={"error": "Некорректно введены дата и время"},
                                         status_code=status.HTTP_400_BAD_REQUEST)
-        else:
-            raise MyCustomException(detail={"error": "Некорректно введены дата и время"},
+        except Exception as e:
+            raise MyCustomException(detail={"error": e.__str__()},
                                     status_code=status.HTTP_400_BAD_REQUEST)
 
 
@@ -452,8 +456,8 @@ class UserRemoveMeetingAPIView(generics.UpdateAPIView, generics.RetrieveAPIView)
                                         status_code=status.HTTP_400_BAD_REQUEST)
 
             return self.update(request, *args, **kwargs)
-        except:
-            raise MyCustomException(detail={"error": "Введены не корректные данные"},
+        except Exception as e:
+            raise MyCustomException(detail={"error": e.__str__()},
                                     status_code=status.HTTP_400_BAD_REQUEST)
 
 
