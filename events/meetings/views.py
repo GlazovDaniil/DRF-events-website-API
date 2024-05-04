c6ce5b8a282b3ec490141a470943825d0135ed89
GlazovDaniil <d.glazow2016@yandex.ru>
2024-05-04 19:25:01 +0300

    0.2.7.20

--- events/meetings/views.py
+++ events/meetings/views.py
@@ -56,6 +56,7 @@ class MeetingAPIView(generics.ListAPIView):
 
     def get_queryset(self):
         search = self.request.query_params.get("search")
+        print(search)
         if search:
             queryset = self.model.objects.filter(title__startswith=search)
         else:
