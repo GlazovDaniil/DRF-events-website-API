ac128d1cc3b03659dbd7b57e320c7def1d25fe70
GlazovDaniil <d.glazow2016@yandex.ru>
2024-05-18 21:51:50 +0300

    0.2.7.28.1

--- events/meetings/pagination.py
+++ events/meetings/pagination.py
@@ -35,7 +35,7 @@ class StandardResultsSerPagination(PageNumberPagination):
 
 
 class MeetingsPagination(PageNumberPagination):
-    PAGE_SIZE = 2
+    page_size = 2
     page_size_query_param = 'page_size'
 
     max_page_size = 1000
@@ -49,9 +49,9 @@ class MeetingsPagination(PageNumberPagination):
         if self.get_previous_link() is not None:
             previous_link = self.get_previous_link()
 
-        page_count = self.page.paginator.count // self.PAGE_SIZE
-        if self.page.paginator.count % self.PAGE_SIZE != 0:
-            page_count = self.page.paginator.count // self.PAGE_SIZE + 1
+        page_count = self.page.paginator.count // self.page_size
+        if self.page.paginator.count % self.page_size != 0:
+            page_count = self.page.paginator.count // self.page_size + 1
         return Response({
             'links': {
                 'next': next_link,
@@ -60,7 +60,7 @@ class MeetingsPagination(PageNumberPagination):
             'meta': {
                 'current_page':  self.page.number,  # сейчас
                 'page_count': page_count,
-                'pre_page': self.PAGE_SIZE,  # кол эл на странице
+                'pre_page': self.page_size,  # кол эл на странице
                 'total_count': self.page.paginator.count,
             },
             'results': data
